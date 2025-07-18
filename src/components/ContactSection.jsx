c602a538774e17e9633103aedb7e69dd43be3042
S15jn <agjahanvi@gmail.com>
2025-07-18 21:48:53 +0530

    adding new

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -19,38 +19,11 @@ export const ContactSection = () => {
 
 
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-  setIsSubmitting(true);
 
-  emailjs
-    .sendForm(
-      // import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      // import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
-      // e.target,
-      import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-    )
-    .then(
-      (result) => {
-        toast({
-          title: "Message sent!",
-          description: "Thank you for your message. I'll get back to you soon.",
-        });
-        setIsSubmitting(false);
-      },
-      (error) => {
-        toast({
-          title: "Error",
-          description: "Something went wrong. Please try again later.",
-        });
-        setIsSubmitting(false);
-      }
-    );
-};
 
   return (
-    <section id="contact" className="py-24 px-4 relative bg-secondary/30">
-      <div className="container mx-auto max-w-5xl">
+    <section id="contact" className="py-24 px-4 relative bg-secondary/30 ">
+      <div className="container mx-auto max-w-5xl ">
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">
           Get In <span className="text-primary"> Touch</span>
         </h2>
@@ -60,20 +33,20 @@ const handleSubmit = (e) => {
           I'm always open to discussing new opportunities.
         </p>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
+        <div className="text-center ">
           <div className="space-y-8">
             <h3 className="text-2xl font-semibold mb-6">
               {" "}
               Contact Information
             </h3>
 
-            <div className="space-y-6 justify-center">
-              <div className="flex items-start space-x-4">
+            <div className="space-y-6">
+              <div className="flex  justify-center  space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
                   <Mail className="h-6 w-6 text-primary" />{" "}
                 </div>
                 <div>
-                  <h4 className="font-medium"> Email</h4>
+                  <h4 className="font-medium "> Email</h4>
                   <a
                     href="mailto:jsomya068@gmail.com"
                     className="text-muted-foreground hover:text-primary transition-colors"
@@ -82,7 +55,7 @@ const handleSubmit = (e) => {
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
+              <div className="flex items-start justify-center space-x-4">
                 <div className="p-3 rounded-full bg-primary/10">
                   <Phone className="h-6 w-6 text-primary" />{" "}
                 </div>
@@ -96,7 +69,7 @@ const handleSubmit = (e) => {
                   </a>
                 </div>
               </div>
-              <div className="flex items-start space-x-4">
+              <div className="flex items-start space-x-4  justify-center">
                 <div className="p-3 rounded-full bg-primary/10">
                   <MapPin className="h-6 w-6 text-primary" />{" "}
                 </div>
@@ -127,78 +100,7 @@ const handleSubmit = (e) => {
             </div>
           </div>
 
-          <div
-            className="bg-card p-8 rounded-lg shadow-xs"
-            onSubmit={handleSubmit}
-          >
-            <h3 className="text-2xl font-semibold mb-6"> Send a Message</h3>
-
-            <form className="space-y-6">
-              <div>
-                <label
-                  htmlFor="name"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Name
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="abc..."
-                />
-              </div>
-
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Email
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary"
-                  placeholder="abc@gmail.com"
-                />
-              </div>
-
-              <div>
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-medium mb-2"
-                >
-                  {" "}
-                  Your Message
-                </label>
-                <textarea
-                  id="message"
-                  name="message"
-                  required
-                  className="w-full px-4 py-3 rounded-md border border-input bg-background focus:outline-hidden foucs:ring-2 focus:ring-primary resize-none"
-                  placeholder="Hello, I'd like to talk about..."
-                />
-              </div>
-
-              <button
-                type="submit"
-                disabled={isSubmitting}
-                className={cn(
-                  "cosmic-button w-full flex items-center justify-center gap-2"
-                )}
-              >
-                {isSubmitting ? "Sending..." : "Send Message"}
-                <Send size={16} />
-              </button>
-            </form>
-          </div>
+         
         </div>
       </div>
     </section>
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,26 +1,46 @@
 import { ArrowRight, ExternalLink, Github } from "lucide-react";
 
 const projects = [
-  // {
-  //   id: 1,
-  //   title: "AgganBreeze",
-  //   description: "Developed a full-stack hotel booking platform that allows users to register, log in securely, explore hotel listings, and manage bookings through a user-friendly interface. Built using Node.js, Express, MongoDB, and EJS with features like session management, form validation, and flash messaging.",
-  //   image: "/projects/AgganBreeze.png",
-  //   tags: ["Node.js", "Express", "MongoDB","EJS","JavaScript","BootStrap"],
-  //   demoUrl: "https://agganbreeze.onrender.com/listings",
-  //   githubUrl: "https://github.com/S15jn/WanderLust",
-  // },
+ 
   {
-    id: 2,
+    id: 1,
     title: "AgganBreeze",
     description: "Developed a full-stack hotel booking platform that allows users to register, log in securely, explore hotel listings, and manage bookings through a user-friendly interface. Built using Node.js, Express, MongoDB, and EJS with features like session management, form validation, and flash messaging.",
     image: "/projects/AgganBreeze.png",
     tags: ["Node.js", "React","Material UI","JavaScript","API's"],
+    demoUrl: "https://agganbreeze.onrender.com/listings",
+      githubUrl: "https://github.com/S15jn/WanderLust/tree/main",
+  },
+  {
+    id: 2,
+    title: "Vroom",
+    description: "Vroom is a user-friendly Car Rental System developed in Java using Object-Oriented Programming. It enables customers to easily rent and return cars through a simple, interactive interface.",
+    image: "/projects/vroom.png",
+    tags: ["Java"],
+    demoUrl: "https://github.com/S15jn/Vroom",
+      githubUrl: "https://github.com/S15jn/Vroom",
+  },
+  {
+    id: 3,
+    title: "Facial Emotion Detection",
+    description: "A Deep Learning-based Facial Emotion Detection system that classifies human emotions from images and webcam feed using Convolutional Neural Networks (CNN) and OpenCV.",
+     image: "/projects/happy.jpg",
+    tags: ["Python","Keras","TensorFlow","OpenCV","CNN","NumPy","Pandas","Jupyter Notebook"],
+    demoUrl: "https://github.com/S15jn/Facial-Emotion-Detection-using-Python-CNN",
+      githubUrl: "https://github.com/S15jn/Facial-Emotion-Detection-using-Python-CNN",
+  },
+   {
+    id: 4,
+    title: "WeatherApp",
+    description:
+      "Weather App is a sleek, user-friendly React app with Material-UI that delivers real-time weather updates worldwide. It dynamically displays temperature, humidity, and more with visually appealing backgrounds",
+    image: "/projects/weather.png",
+    tags: ["ReactJs", "JavaScript","MaterialUI","Fetch API" ,"Html","CSS","OpenWeatherMap API"],
     demoUrl: "https://weather-app-dun-eight-35.vercel.app/",
     githubUrl: "https://github.com/S15jn/WeatherApp",
   },
   {
-    id: 3,
+    id: 5,
     title: "OCHI: Dynamic Landing Page with Seamless Animations",
     description:
       "OCHI is a vibrant, interactive landing page designed with smooth animations to captivate users and enhance their browsing experience.",
@@ -29,18 +49,9 @@ const projects = [
     demoUrl: "https://ochi-project-nine.vercel.app/",
     githubUrl: "https://github.com/S15jn/ochi_project",
   },
+ 
   {
-    id: 4,
-    title: "WeatherApp",
-    description:
-      "Weather App is a sleek, user-friendly React app with Material-UI that delivers real-time weather updates worldwide. It dynamically displays temperature, humidity, and more with visually appealing backgrounds",
-    image: "/projects/weather.png",
-    tags: ["ReactJs", "JavaScript","MaterialUI","Fetch API" ,"Html","CSS","OpenWeatherMap API"],
-    demoUrl: "https://ochi-project-nine.vercel.app/",
-    githubUrl: "https://github.com/S15jn/ochi_project",
-  },
-  {
-    id: 5,
+    id:6,
     title: "Todo App",
     description:
       "A dynamic and interactive to-do list application that allows users to easily manage their tasks, including adding, deleting, checking, and unchecking items to enhance productivity and organization.",
@@ -79,7 +90,14 @@ export const ProjectsSection = () => {
                 />
               </div>
 
-              <div className="p-6">
+            
+
+                <h3 className="text-xl font-semibold mt-2 mb-4"> {project.title}</h3>
+                <p className="text-muted-foreground text-sm mb-4">
+                  {project.description}
+                </p>
+
+                  <div className="p-6">
                 <div className="flex flex-wrap gap-2 mb-4">
                   {project.tags.map((tag) => (
                     <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
@@ -87,11 +105,6 @@ export const ProjectsSection = () => {
                     </span>
                   ))}
                 </div>
-
-                <h3 className="text-xl font-semibold mb-1"> {project.title}</h3>
-                <p className="text-muted-foreground text-sm mb-4">
-                  {project.description}
-                </p>
                 <div className="flex justify-between items-center">
                   <div className="flex space-x-3">
                     <a
